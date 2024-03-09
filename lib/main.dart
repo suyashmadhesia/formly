620108bb4be5c2b0f23ea8c36235e87c7a4c7bf3
Suyash Maddhessiya <suyashmadhesia@gmail.com>
2024-03-09 18:43:33 +0530

    project initiated

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FormlyApp());
+}
+
+class FormlyApp extends StatelessWidget {
+  const FormlyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
